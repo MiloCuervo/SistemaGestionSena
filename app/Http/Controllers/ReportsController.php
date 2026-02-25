2464721edbe8090136ed54a9b830fbaab7f0d7bb
MiloCuervo <camilo.cuervojobs@gmail.com>
2026-02-25 11:15:30 -0500

    Cazc Creating Charts for reports

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -6,23 +6,4 @@ use Illuminate\Http\Request;
 
 class ReportsController extends Controller
 {
-    public function __invoke()
-    {
-        $cases = Cases::with("user", "contact", "organizationProcess")->get();
-        return view('admin.reports', compact('cases'));
-    }
-
-
-    public function dataCharts(){
-        $stats = [
-            'total' => Cases::count(),
-            'attended' => Cases::where('status', 'attended')->count(),
-            'in_progress' => Cases::where('status', 'in_progress')->count(),
-            'not_attended' => Cases::where('status', 'not_attended')->count(),
-        ];
-
-        
-       
-        ]
-    }
 }
