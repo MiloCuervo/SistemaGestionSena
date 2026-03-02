a781acaa8af1c7d37c52000abfabff5c58728d19
Jhonatan <jhonatan27cv@gmail.com>
2026-03-02 10:14:19 -0500

    adding ShouldQueue to notifications

--- app/Notifications/CaseCreatedMail.php
+++ app/Notifications/CaseCreatedMail.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class CaseCreatedMail extends Notification
+class CaseCreatedMail extends Notification implements ShouldQueue
 {
     use Queueable;
 
--- app/Notifications/CaseCreatedNotification.php
+++ app/Notifications/CaseCreatedNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class CaseCreatedNotification extends Notification
+class CaseCreatedNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
